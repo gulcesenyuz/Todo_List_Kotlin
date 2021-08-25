b94c573ae8f44fb0b6aee87309588ded48338ba7
gülce <gizemgulce@gmail.com>
2021-08-25 15:43:50 +0300

    text missing

---
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private  lateinit var todoAdapter: TodoAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        todoAdapter= TodoAdapter(mutableListOf())
+
+        rvToDoItems.adapter=todoAdapter
+        rvToDoItems.layoutManager=LinearLayoutManager(this)
+
+        btnAddToDo.setOnClickListener {
+            val todoTitle= etToDoTitle.text.toString()
+            if (todoTitle.isNotEmpty()){
+                val todo=Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                etToDoTitle.text.clear()
+            }
+        }
+btnDeleteToDo.setOnClickListener {
+    todoAdapter.deleteDoneTodos()
+}
+
+    }
+}
+
